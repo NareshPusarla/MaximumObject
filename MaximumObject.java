0a2227a36b9b9006bbc3d4996822f438c291cc38
NareshPusarla <86452410+NareshPusarla@users.noreply.github.com>
2021-07-14 20:24:53 +0530

    Uc5

--- MaximumObject.java
+++ MaximumObject.java
@@ -1,9 +1,21 @@
 package com.bridgelabz.generics;
 
 public class MaximumObject<T extends Comparable<T>> {
+	T x,y,z;
+	
+	public MaximumObject(T x, T y, T z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public T maximum() {
+		return MaximumObject.maximum(x, y, z);
+	}
 	
 	public static <T extends Comparable<T>> T maximum(T x,T y,T z) {
-		
+
 		T max = x;
 		if(y.compareTo(max)>0) {
 			max = y;
@@ -15,19 +27,35 @@ public class MaximumObject<T extends Comparable<T>> {
 		return max;
 	}
 
+	public static String testMaximum(String x, String y, String z) {
+		String max = x;
+		if(y.compareTo(max)>0) {
+			max = y;
+		}
+		if(y.compareTo(max)>0) {
+			max=z;
+		}
+		printMax(x,y,z,max);
+		return max;
+	}
+	
 	public static <T> void printMax(T x, T y, T z, T max) {
 		System.out.println("Max of these is\n"+x+" "+y+" "+z+" "+max);
 	}
 	
 	public static void main(String[] args) {
-		MaximumObject maxObject = new MaximumObject();
-		maxObject.maximum(4,5,6);
+		Integer xi=4, yi=5, zi=6;
+		Float xf=4.6f, yf=5.5f ,zf=6.7f;
+		String xs="Apple", ys="Peach", zs="Banana";
 		
-		MaximumObject maxObject1 = new MaximumObject();
-		maxObject1.maximum(4.1,5.2,6.3);
+		MaximumObject maxObject = new MaximumObject(xi, yi, zi);
+		maxObject.maximum();
 		
-		MaximumObject maxObject2 = new MaximumObject();
-		maxObject2.maximum("Apple","Banana","Peach");
+		MaximumObject maxObject1 = new MaximumObject(xf, yf, zf);
+		maxObject1.maximum();
+		
+		MaximumObject maxObject2 = new MaximumObject(xs, ys, zs);
+		maxObject2.maximum();
 	}
 
 }
