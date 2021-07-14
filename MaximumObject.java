b5ecfd5351f559970f7ee73fd475240dfd36657f
NareshPusarla <86452410+NareshPusarla@users.noreply.github.com>
2021-07-14 18:07:59 +0530

    Uc3

--- MaximumObject.java
+++ MaximumObject.java
@@ -1,10 +1,9 @@
 package com.bridgelabz.generics;
 
 public class MaximumObject {
-	int x,y,z,max; 
-	float xf,yf,zf,maxf;
+	
 	public int maximum(int x,int y,int z) {
-		max = x;
+		int max = x;
 		if(y>max) {
 			max = y;
 		}
@@ -16,15 +15,27 @@ public class MaximumObject {
 	}
 	
 	public float maximum1(float xf,float yf,float zf) {
-		maxf = xf;
+		float maxf = xf;
 		if(yf>maxf) {
 			maxf = yf;
 		}
-		if(z>maxf) {
+		if(zf>maxf) {
 			maxf=zf;
 		}
 		printMax1(xf,yf,zf,maxf);
 		return maxf;
+	}
+	
+	public String maximum2(String xs, String ys, String zs) {
+		String maxs = xs;
+		if(ys.compareTo(maxs)>0) {
+			maxs = ys;
+		}
+		if(ys.compareTo(maxs)>0) {
+			maxs=zs;
+		}
+		printMax2(xs,ys,zs,maxs);
+		return maxs;
 	}
 	
 	public static void printMax(int x, int y, int z, int max) {
@@ -35,12 +46,19 @@ public class MaximumObject {
 		System.out.println("Max of %s, %s and %s is %s\n"+xf+" "+yf+" "+zf+" "+maxf);
 	}
 
+	public static void printMax2(String xs, String ys, String zs, String maxs) {
+		System.out.println("Max of these string is\n"+xs+" "+ys+" "+zs+" "+maxs);
+	}
+	
 	public static void main(String[] args) {
 		MaximumObject maxObject = new MaximumObject();
 		maxObject.maximum(4, 5, 6);
 		
 		MaximumObject maxObject1 = new MaximumObject();
-		maxObject1.maximum1(4.2f, 5.3f, 6.4f);
+		maxObject1.maximum1(4.2f, 6.4f, 5.3f);
+		
+		MaximumObject maxObject2 = new MaximumObject();
+		maxObject2.maximum2("Apple","Peach","Banana");
 	}
 
 }
